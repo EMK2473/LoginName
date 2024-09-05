e16fba5967a31251bbbc5b900228436322df941e
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-04 23:18:07 -0400

    init file and package setup

---
+++ LoginName.java
@@ -0,0 +1,13 @@
+package LoginName;
+
+import java.util.Scanner;
+
+public class LoginName {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+      
+      
+      
+	}
+}
