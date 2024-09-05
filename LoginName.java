36908006397f09355e0f7881a91063e2eca9cb12
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 00:00:02 -0400

    added conditional check for proper input format

--- LoginName.java
+++ LoginName.java
@@ -6,12 +6,24 @@ public class LoginName {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		
+		String input = scnr.nextLine();
+		String[] data = input.split(" ");
+		
+		if(data.length < 3){
+		   System.out.println("Please input your information in the format: FirstName LastName YearBorn");   
+		   scnr.close();
+		   return;
+		} 
+		
+        
+		String firstName = data[0].length() >= 6 ? data[0].substring(0, 6) : data[0];
+		
+		String lastNameInitial = data[1].substring(0, 1);
+		String year = data[2];
       
-        String input = scnr.nextLine();
-        String[] data = input.split(" ");
-
-        System.out.println(data[0] + data[1].charAt(0) + "_" + data[data.length - 1].charAt(data.length));;
+      System.out.println("Your login name: " + firstName + "" + lastNameInitial + "_" + year.charAt(data[2].length() - 1));
       
-        scnr.close();
+      scnr.close();
 	}
 }
