a4f607733c6fc8f10c9b9bfa6e3126f0cbec51e4
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-04 23:28:46 -0400

    assinged input as string array, split array at each ' ', printed out data in required format

--- LoginName.java
+++ LoginName.java
@@ -7,7 +7,11 @@ public class LoginName {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
       
+        String input = scnr.nextLine();
+        String[] data = input.split(" ");
+
+        System.out.println(data[0] + data[1].charAt(0) + "_" + data[data.length - 1].charAt(data.length));;
       
-      
+        scnr.close();
 	}
 }
